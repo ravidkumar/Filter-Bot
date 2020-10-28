d127b926453abbbdaedcdec9d078b7577660097e
ravidkumar <71187213+ravidkumar@users.noreply.github.com>
2020-10-28 09:10:59 +0100

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -161,7 +161,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             send_start(bot, update)
     else:
         update.effective_message.reply_text("Heya,{} Here..\nHow can I help you? 🙂".format(bot.first_name),reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="help",url="t.me/{}?start=help".format(bot.username))]]))
+                                                [[InlineKeyboardButton(text="𝗛𝗘𝗟𝗣 📲",url="t.me/{}?start=help".format(bot.username))]]))
 
 def send_start(bot, update):
     #Try to remove old message
@@ -175,7 +175,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="help",callback_data="help_back"),InlineKeyboardButton(text="𝗢𝗪𝗡𝗘𝗥 🧕",url="https://t.me/Iggie")]]
+    keyboard = [[InlineKeyboardButton(text="𝗛𝗘𝗟𝗣 📲",callback_data="help_back"),InlineKeyboardButton(text="𝗢𝗪𝗡𝗘𝗥 🧕",url="https://t.me/Iggie")]]
     keyboard += [[InlineKeyboardButton(text="𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗚𝗥𝗢𝗨𝗣 👥", callback_data="main_connect"),InlineKeyboardButton(text="𝗔𝗗𝗗 𝗠𝗘 ➕",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
@@ -276,7 +276,7 @@ def get_help(bot: Bot, update: Update):
 
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="help",url="t.me/{}?start=help".format(bot.username))],  
+                                                [[InlineKeyboardButton(text="𝗛𝗘𝗟𝗣 📲",url="t.me/{}?start=help".format(bot.username))],  
                                                 [InlineKeyboardButton(text="𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗢𝗪𝗡𝗘𝗥 🧕",url="https://t.me!Iggie")]]))
         return
 
